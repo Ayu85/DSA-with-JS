a791077ff5a4eb6b2fba474f1856121267a54b04
Ayu85 <ayushsinghdalimss@gmail.com>
2024-12-03 22:16:28 +0530

    build in methods

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,6 @@
+// using built in methods
 const isPalindrome = function (num) {
   return num === +num.toString().split("").reverse().join("");
 };
 console.log(isPalindrome(121));
-
+// using for loop
