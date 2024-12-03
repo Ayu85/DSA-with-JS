d75d71039e9f79597e4eced36e37efdad24107e9
Ayu85 <ayushsinghdalimss@gmail.com>
2024-12-03 22:28:34 +0530

    using loops

--- palindrome.js
+++ palindrome.js
@@ -2,5 +2,20 @@
 const isPalindrome = function (num) {
   return num === +num.toString().split("").reverse().join("");
 };
-console.log(isPalindrome(121));
+// console.log(isPalindrome(121));
 // using for loop
+function isPalindrome2(num) {
+  const rev = reverseNumber(num);
+  console.log(rev === num);
+   
+}
+function reverseNumber(num) {
+  let rev = 0;
+  while (num > 0) {
+    let rem = num % 10;
+    rev = rev * 10 + rem;
+    num = Math.floor(Math.min(num / 10));
+  }
+  return rev;
+}
+isPalindrome2(245321);
